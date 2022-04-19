6ccc5d0600738857cf8efe90d2257cb1254f3635
Igor Molina <igordsmolina@gmail.com>
2022-04-19 14:15:08 -0300

    :D

--- src/SuperFatorial/CalculateSuperFatorial.java
+++ src/SuperFatorial/CalculateSuperFatorial.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class CalculateSuperFatorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int choosen;
 
         System.out.println("1-Fatorial with Looping");
         System.out.println("2-Fatorial with dinamic programming");
-        choosen = sc.nextInt();
+        int choose = sc.nextInt();
 
-        switch (choosen){
+        switch (choose){
             case 1:
                superFatorialWithLooping();
                break;
@@ -25,12 +24,11 @@ public class CalculateSuperFatorial {
     static void superFatorialWithLooping() {
         Scanner sc =  new Scanner(System.in);
 
-        int number;
         double finalResult = 1;
-        long tempoInicio = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 
         System.out.println("De qual número você deseja receber o super fatorial?");
-        number = sc.nextInt();
+        int number = sc.nextInt();
 
         for (int j = number; j >=1; j--) {
             for (int i = j; i >= 1; i--) {
@@ -38,29 +36,33 @@ public class CalculateSuperFatorial {
             }
         }
         System.out.println(finalResult);
-        System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
+        System.out.println("Tempo Total: " + (System.currentTimeMillis() - start));
 
         sc.close();
     }
 
     static void superFatorialWithDinamicProgramming() {
         Scanner sc =  new Scanner(System.in);
-        double number;
         long tempoInicio = System.currentTimeMillis();
 
         System.out.println("De qual número você deseja receber o super fatorial?");
-        number = sc.nextInt();
+        int number = sc.nextInt();
 
-        double result = calculateSuperFatorial(number);
+        int result = conclusion(number);
         System.out.println("Result: " + result);
         System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
     }
 
-    private static double calculateSuperFatorial(double number) {
-        if (number >= 1) {
-            return number *= calculateSuperFatorial(number - 1);
-        } else {
+    private static int superFatorial(int number, int number2){
+        if(number2 == 1){
             return 1;
         }
+        if (number == 1){
+            return superFatorial(number2 - 1, number2 - 1);
+        }
+        return number * superFatorial(number - 1, number2);
+    }
+    private static int conclusion(int number){
+        return superFatorial(number, number);
     }
 }
