285366ae0525c657a02bfc60a178224d924f7718
Matheus-Reinert <reinert.matheus@hotmail.com>
2022-04-19 19:56:59 -0300

    Adicionando consumo de memória

--- src/SuperFatorial/CalculateSuperFatorial.java
+++ src/SuperFatorial/CalculateSuperFatorial.java
@@ -6,9 +6,7 @@ public class CalculateSuperFatorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("1-Fatorial with Looping");
-        System.out.println("2-Fatorial with dinamic programming");
-        int choose = sc.nextInt();
+        int choose = 1;
 
         switch (choose){
             case 1:
@@ -22,35 +20,42 @@ public class CalculateSuperFatorial {
     }
 
     static void superFatorialWithLooping() {
-        Scanner sc =  new Scanner(System.in);
 
         double finalResult = 1;
         long start = System.currentTimeMillis();
 
-        System.out.println("De qual número você deseja receber o super fatorial?");
-        int number = sc.nextInt();
+        int number = 4;
 
         for (int j = number; j >=1; j--) {
             for (int i = j; i >= 1; i--) {
                 finalResult *= i;
             }
         }
+
+        System.out.println("SuperFatorial de: " + number);
         System.out.println(finalResult);
         System.out.println("Tempo Total: " + (System.currentTimeMillis() - start));
-
-        sc.close();
+        System.out.println("Meg used= "+(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/(1000*1000)+"M");
     }
 
     static void superFatorialWithDinamicProgramming() {
-        Scanner sc =  new Scanner(System.in);
-        long tempoInicio = System.currentTimeMillis();
 
-        System.out.println("De qual número você deseja receber o super fatorial?");
-        int number = sc.nextInt();
+        int dataSize = 1024 * 1024;
+        long tempoInicio = System.currentTimeMillis();
+        Runtime runtime = Runtime.getRuntime();
+
+        int number = 4;
 
         int result = conclusion(number);
+
+        System.out.println("SuperFatorial de: " + number);
         System.out.println("Result: " + result);
         System.out.println("Tempo Total: "+(System.currentTimeMillis()-tempoInicio));
+        System.out.println("Meg used= "+(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/(1000*1000)+"M");
+    }
+
+    private static int conclusion(int number){
+        return superFatorial(number, number);
     }
 
     private static int superFatorial(int number, int number2){
@@ -61,8 +66,5 @@ public class CalculateSuperFatorial {
             return superFatorial(number2 - 1, number2 - 1);
         }
         return number * superFatorial(number - 1, number2);
-    }
-    private static int conclusion(int number){
-        return superFatorial(number, number);
     }
 }
